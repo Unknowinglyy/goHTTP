9c5c868c1e05badc55729d009c1215922b45f663
Unknowinglyy <unknowingly@tamu.edu>
2025-11-20 17:10:27 -0600

    small udp example

---
+++ cmd/udplistener/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	// gives you a UDPAddr struct which is needed in DialUDP
+	// setting up which addr:port we are sending to
+	addr, err := net.ResolveUDPAddr("udp", ":42069")
+	if err != nil {
+		log.Fatalf("error resolving UDP address: %s", err)
+	}
+
+	// (addrs refer to addr:port)
+	// leave laddr nil as we don't care what port we are sending from on the local machine
+	// arguments -> network, local addr (sending from), remote addr (sending to)
+	udpConn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatalf("error establishing UDP socket: %s", err)
+	}
+	defer udpConn.Close()
+
+	buff := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		line, err := buff.ReadString('\n')
+		if err != nil {
+			log.Fatalf("error reading line: %s", err)
+		}
+
+		_, err = udpConn.Write([]byte(line))
+		if err != nil {
+			log.Fatalf("error writing line to UDP connection: %s", err)
+		}
+	}
+}
